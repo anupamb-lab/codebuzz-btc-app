d28c87215fbdc736025caa7dc44cddd3f2b78a23
vaibhav-xi <vaibhav28890@gmail.com>
2025-09-14 13:11:09 +0800

    ios packages update

--- ios/BitPlayPro/AppDelegate.swift
+++ ios/BitPlayPro/AppDelegate.swift
@@ -76,7 +76,7 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
 
   override func bundleURL() -> URL? {
     #if DEBUG
-      return URL(string: "https://b9998c289e7c.ngrok-free.app/index.bundle?platform=ios&dev=true")
+      return URL(string: "https://e046f492b95e.ngrok-free.app/index.bundle?platform=ios&dev=true")
     #else
       return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
     #endif
